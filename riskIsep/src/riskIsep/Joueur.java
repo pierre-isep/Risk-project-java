d47a31e29c0fb0fb1661134c6cfe05b1030d15f5
JoffreyBourguet <33147009+JoffreyBourguet@users.noreply.github.com>
2018-05-10 14:30:54 +0200

    Add files via upload

---
+++ riskIsep/src/riskIsep/Joueur.java
@@ -0,0 +1,18 @@
+package riskIsep;
+
+public class Joueur {
+	int numJoueur;
+	int armee;
+	int territoiresControles; //a modifier en tab
+	int regionsControles;
+	
+	public Joueur(int numJoueur, int armee, int territoiresControles, int regionsControles) {
+		this.numJoueur = numJoueur;
+		this.armee = armee;
+		this.territoiresControles = territoiresControles;
+		this.regionsControles = regionsControles;
+	}
+	
+	
+
+}
---
+++ riskIsep/src/riskIsep/PartieRisk.java
@@ -0,0 +1,22 @@
+package riskIsep;
+
+public class PartieRisk {
+	
+	public static void main (String [] args){
+		Region asie = new Region("Asie",12);
+		Region ameriqueNord = new Region("Amerique du Nord",9);
+		Region europe = new Region("Europe",7);
+		Region afrique = new Region("Afrique",6);
+		Region oceanie = new Region("Oceanie",4);
+		Region ameriqueSud = new Region("Amerique du Sud",4);
+		int nbJoueurs=2;
+	
+		Joueur J1 = new Joueur(1,40,21,0);
+		Joueur J2 = new Joueur(2,40,21,0);
+
+
+		//for (int k=42, k>0; k--) { Distribuer aléatoirement les territoires
+		//
+		//}
+	}
+}
